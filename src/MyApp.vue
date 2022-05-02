99052e07fa871f1b196b81c6f0e66feda889ac07
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-05-02 14:03:18 +0700

    feat: add antialiased on index.html and add padding x to home page

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center">
-    <h1 class="text-lg font-semibold text-slate-700">
+  <div class="flex min-h-screen items-center justify-center px-4">
+    <h1 class="text-center text-xl font-semibold text-slate-700">
       <span class="text-cyan-400">todo.fahmiidris.dev</span> currently under development!
     </h1>
   </div>
